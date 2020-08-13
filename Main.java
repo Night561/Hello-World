28d6d680a3113d60cc6c7663e35e22e8a3d49e63
Night561 <68205821+Night561@users.noreply.github.com>
2020-08-13 12:27:18 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("I go to school at Milpitas High School.");
+		System.out.println("I enjoy fighting games a ton.");
+		System.out.println("My least favorite part of fighting games is building the controller.");
+		System.out.println("I'm not sure what I want to major in just yet,");
+		System.out.println("but I want to do something I have fun with.");
+	}
+}
